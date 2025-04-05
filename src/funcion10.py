21c303f4e4df828245d11ff7877fbdc6f6a7bc23
Santiago Alvarez <santiiariel2004@gmail.com>
2025-04-04 23:52:56 -0300

    Modifico ejercicio10

--- src/funcion10.py
+++ src/funcion10.py
@@ -5,28 +5,6 @@ actions_points = {
     'deaths' : -1
 }
 
-puntos_jugadores = {
-'Shadow': {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp' : 0, 'puntos' : 0},
-'Blaze': {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp' : 0, 'puntos' : 0},
-'Viper': {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp' : 0, 'puntos' : 0},
-'Frost': {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp' : 0, 'puntos' : 0},
-'Reaper': {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp' : 0, 'puntos' : 0}
-}
-
-
-
-#formato = f"Ranking ronda {ronda} \n jugador    kills   asistencias     Muertes    Puntos "
-
-"""
-    recorro diccionaario -> recorro por jugador -> de cada jugador recorro sus acciones
-    sumar_actions -> sumo las acciones para el ranking total // Ignoro mvp y puntos
-    sumar_puntos -> sumo puntos ganados en esta ronda con las anteriores
-    MVPs -> como obtengo el mvp del la ronda?
-
-"""
-
-
- 
 #Menos El MVP 
 def sumar_actions (jugador, actions, puntos_jugadores) : 
      global actions_points
@@ -36,15 +14,13 @@ def sumar_actions (jugador, actions, puntos_jugadores) :
          #round_point[jugador][action] = value #Muerte True = 1 and False = 0
          puntos_jugadores[jugador][action] += value 
          point += actions_points[action] * value
+
      puntos_jugadores[jugador]["puntos"] += point
      return point
 
-
+#junto con MVPs
 def imprimir_ronda(jugador, actions) :
      cadena = jugador
-     point = 0
-     mvp = ""
-     max_point = 0
      for value in actions.values() :
          cadena += f"{" "*7}{value}"
      print(cadena)
